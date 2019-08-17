1fa2af70c5a97eeae8f1d13bfe8257fc1597ff50
jeffreytram <jeffrey_tram@yahoo.com>
2019-08-16 20:44:38 -0400

    Added more info link and location display

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Weather-App
+# Runner's Weather App
+Created by: Jeffrey Tram<br/>
+Uses <a href="https://darksky.net/dev">Dark Sky API</a> and <a href="https://nominatim.org/release-docs/develop/api/Overview/">Nominatim API</a><br/>
+
--- script.js
+++ script.js
@@ -3,16 +3,34 @@ window.addEventListener('load', () => {
     let lat;
     // degree, humidity, dew point
 
+    let location = document.querySelector('.location');
     let statusReport = document.querySelector('.status');
     let temperatureReport = document.querySelector('.temperature');
     let humidityReport = document.querySelector('.humidity');
     let dewpoint = document.querySelector('.dewpoint');
+    let moreInfoLink = document.querySelector('.link');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             console.log(position);
             lat = position.coords.latitude;
             long = position.coords.longitude;
+
+            moreInfoLink.href = `https://darksky.net/forecast/${lat},${long}/`;
+
+            let locationAPI = `https://nominatim.openstreetmap.org/search.php?q=${lat}%2C${long}&format=json`;
+            //what if non US location
+            //let locationAPI = `https://nominatim.openstreetmap.org/search.php?q=48.118219, 11.630618&format=json`;
+            fetch(locationAPI)
+                .then(response => {
+                    return response.json();
+                })
+                .then(details => {
+                    console.log(details);
+                    const { display_name } = details[0];
+                    location.textContent = display_name;
+
+                })
 
             const proxy = 'https://cors-anywhere.herokuapp.com/';
             const api = `${proxy}https://api.darksky.net/forecast/e2a300014df353c32e5265bac28a1a7e/${lat},${long}`;
