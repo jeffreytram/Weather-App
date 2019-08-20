50223f4a57b763ecfcf6641648be9b2473d2c29b
jeffreytram <jeffrey_tram@yahoo.com>
2019-08-20 08:41:28 -0400

    Several minor changes and improvements Added tab click event listener Now refreshes current weather when clicking current Added more info for past Now displays error message for invalid date or time input Added pointer on hover of tabs and buttons

--- script.js
+++ script.js
@@ -5,13 +5,37 @@ window.addEventListener('load', () => {
         navigator.geolocation.getCurrentPosition(position => {
             lat = position.coords.latitude;
             long = position.coords.longitude;
-            generateWeather(lat, long, -1)
+            
+            generateWeather(lat, long, -1);
         });
     }
     revealPastOptions(false);
     document.getElementById("inputSubmit").addEventListener('click', function () {
-        generateWeather(lat, long, getUserDateInput());
+        try{
+            clearErrorMessage();
+            generateWeather(lat, long, getUserDateInput());
+        }
+        catch{
+            displayErrorMessage();
+        }
     });
+    document.getElementById("current").addEventListener('click', function () {
+        generateWeather(lat, long, -1)
+        revealPastOptions(false);
+        setActive("current");
+    });
+    document.getElementById("past").addEventListener('click', function () {
+        revealPastOptions(true);
+        setActive("past")
+    });
+    function setActive(tabToSet) {
+        let activeTab = document.getElementsByClassName("active");
+        if (activeTab.length > 0) {
+            activeTab[0].className = activeTab[0].className.replace(" active", "");
+        }
+        let newActiveTab = document.getElementById(tabToSet);
+        newActiveTab.className += " active";
+    }
 });
 
 function generateWeather(lat, long, time) {
@@ -40,7 +64,11 @@ function generateWeather(lat, long, time) {
     if (time != -1) {
         //api = `${proxy}https://api.darksky.net/forecast/e2a300014df353c32e5265bac28a1a7e/${lat},${long},${time}`;
         api = `${proxy}https://api.darksky.net/forecast/e2a300014df353c32e5265bac28a1a7e/${lat},${long},${time}`;
-        moreInfoLink.href = ``;
+
+        let inputDate = document.getElementsByClassName("dateInput");
+        let formattedDate = inputDate[0].value;
+
+        moreInfoLink.href = `https://darksky.net/details/${lat},${long}/${formattedDate}`;
     }
     else {
         //current
@@ -57,9 +85,10 @@ function generateWeather(lat, long, time) {
             const { summary, temperature, humidity, dewPoint, icon } = data.currently;
 
             statusReport.textContent = summary;
-            temperatureReport.textContent = "temp: " + temperature;
-            humidityReport.textContent = "humidity: " + humidity * 100 + "%";
-            dewpoint.textContent = "dew point: " + dewPoint;
+            debugger;
+            temperatureReport.textContent = "temp: " + Math.round(temperature) + "°F";
+            humidityReport.textContent = "humidity: " + Math.round(humidity * 100) + "%";
+            dewpoint.textContent = "dew point: " + Math.round(dewPoint) + "°F";
 
             setIcons(icon, document.querySelector('.icon'));
 
@@ -85,21 +114,31 @@ function getUserDateInput() {
     let dateInput = document.getElementsByClassName("dateInput");
     let timeInput = document.getElementsByClassName("timeInput");
 
-    let formattedTime = dateInput[0].value +"T"+timeInput[0].value+":00";
+    let formattedTime = dateInput[0].value + "T" + timeInput[0].value + ":00";
 
     //in minutes
     let offsetMin = dateInput[0].valueAsDate.getTimezoneOffset() //240
-    let offsetHour = Math.abs(offsetMin/60); //4
+    let offsetHour = Math.abs(offsetMin / 60); //4
     let offsetMod = Math.abs(offsetMin % 60);
 
-    let sign = (offsetMin>0)?"-":"+";
-    let hour = (offsetHour<10?"0"+offsetHour:offsetHour);
-    let min = (offsetMod<10?"0"+offsetMod:offsetMod);
+    let sign = (offsetMin > 0) ? "-" : "+";
+    let hour = (offsetHour < 10 ? "0" + offsetHour : offsetHour);
+    let min = (offsetMod < 10 ? "0" + offsetMod : offsetMod);
 
-    formattedTime += sign+hour+min;
+    debugger;
+    formattedTime += sign + hour + min;
+    console.log(formattedTime);
+
     //Thu Jun 20 2019 20:00:00 GMT-0400 (Eastern Daylight Time)
-    //how to get offset
     //[YYYY]-[MM]-[DD]T[HH]:[MM]:[SS][timezone]
     //let unixTime = (dateInput[0].valueAsNumber / 1000) + (timeInput[0].valueAsNumber / 1000);
     return formattedTime;
+}
+function displayErrorMessage(){
+    let errorDiv = document.getElementsByClassName("errorHandling");
+    errorDiv[0].textContent = "Error: Incorrect date and/or time."
+}
+function clearErrorMessage(){
+    let test = document.getElementsByClassName("errorHandling");
+    test[0].textContent = "";
 }
